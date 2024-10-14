3c7b095f8d7342d55f6d648185873e35399249ad
sarkarghya <sarkarghya@gmail.com>
2024-10-14 02:39:59 -0400

    “hw3”

--- gitpull.sh
+++ gitpull.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-cd   #go to your home directory
-
-cd /home/ec2-user/Flask-hands-on  #Customize the local address of your git repo with .git
-
-git pull | tee -a /home/ec2-user/gitpull-info.txt  #Print changes made with gitpull to a gitpull-info.txt file
+git pull | tee -a ./gitpull-info.txt  #Print changes made with gitpull to a gitpull-info.txt file
 
 echo $(date)$'\n'"---------------------------------" >> //home/ec2-user/gitpull-info.txt #add the date of the transaction to the file and go to the bottom line
