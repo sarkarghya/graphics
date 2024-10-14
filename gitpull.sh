74077b8036afa2c1c4fe23f39c437e9bdc9924d0
sarkarghya <sarkarghya@gmail.com>
2024-10-14 02:42:56 -0400

    “hw3”

--- gitpull.sh
+++ gitpull.sh
@@ -2,4 +2,4 @@
 
 git pull | tee -a ./gitpull-info.txt  #Print changes made with gitpull to a gitpull-info.txt file
 
-echo $(date)$'\n'"---------------------------------" >> //home/ec2-user/gitpull-info.txt #add the date of the transaction to the file and go to the bottom line
+echo $(date)$'\n'"---------------------------------" >> ./gitpull-info.txt #add the date of the transaction to the file and go to the bottom line
